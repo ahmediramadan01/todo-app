014c3d7787a6f26286222cd6df05dc99c1b7cf9b
Ahmed Ramadan <143494954+ahmediramadan01@users.noreply.github.com>
2024-12-23 14:04:57 +0200

    Build header section and theme toggling functionality

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 "use script";
+
+/* Element selectors */
+
+const btnThemeToggle = document.querySelector(".btn--theme-toggle");
+
+/* Theme toggle */
+
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme == "dark") {
+  document.body.classList.toggle("dark-theme");
+} else if (currentTheme == "light") {
+  document.body.classList.toggle("light-theme");
+}
+
+btnThemeToggle.addEventListener("click", function () {
+  let theme;
+  if (prefersDarkScheme.matches) {
+    document.body.classList.toggle("light-theme");
+    theme = document.body.classList.contains("light-theme") ? "light" : "dark";
+  } else {
+    document.body.classList.toggle("dark-theme");
+    theme = document.body.classList.contains("dark-theme") ? "dark" : "light";
+  }
+  localStorage.setItem("theme", theme);
+});
